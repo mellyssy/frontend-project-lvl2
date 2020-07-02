dcfa1ec961b1ed1d588036f16abf5ccddce60203
mellyssy <melissa.cohenn@gmail.com>
2020-07-02 09:39:46 +0300

    create a getDiffTree function

---
+++ src/formatters/stylish.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+const stylish = (tree) => {
+  console.log(tree);
+};
+
+export default stylish;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import _ from 'lodash';
 import parseData from './parsers/parsers';
+import getDiffTree from './getDiffTree';
+import stylish from './formatters/stylish';
 
 const gendiff = (pathToFile1, pathToFile2) => {
   const [before, after] = [parseData(pathToFile1), parseData(pathToFile2)];
 
-  const keys = _.union(Object.keys(before), Object.keys(after));
+  const tree = getDiffTree(before, after);
+  const formattedResult = stylish(tree);
 
-  const resultingArr = keys.reduce(
-    (acc, key) => {
-      const beforeValue = _.get(before, key, null);
-      const afterValue = _.get(after, key, null);
-
-      if (!afterValue) {
-        return [...acc, `- ${key}: ${beforeValue}`];
-      }
-
-      if (!beforeValue) {
-        return [...acc, `+ ${key}: ${afterValue}`];
-      }
-
-      if (afterValue !== beforeValue) {
-        return [...acc, `- ${key}: ${beforeValue}`, `+ ${key}: ${afterValue}`];
-      }
-
-      return [...acc, `  ${key}: ${beforeValue}`];
-    },
-    [],
-  );
-
-  return `{\n${resultingArr.join('\n')}\n}`;
+  return `{\n${formattedResult.join('\n')}\n}`;
 };
 
 export default gendiff;
