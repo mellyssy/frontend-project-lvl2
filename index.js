d2ff8bba06bed76bc2d8b9ad2dcf1d050f675915
melyssy <ksenyatokareva@gmail.com>
2020-08-10 23:47:58 +0300

    move parsers file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import parseData from './src/parsers/index.js';
+import parseData from './src/parsers.js';
 import buildDiffTree from './src/diffBuilder.js';
 import formatter from './src/formatters/index.js';
 
