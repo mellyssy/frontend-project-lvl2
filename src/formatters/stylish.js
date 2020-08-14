7fda97b9698ae7e8b2766f0cc6ba074f4405633d
melyssy <ksenyatokareva@gmail.com>
2020-08-14 16:10:50 +0300

    fix typos and levels

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,34 +1,34 @@
 import _ from 'lodash';
 
 const stringifyObject = (obj, lvl) => _.keys(obj).map((key) => {
-  const indent = '  '.repeat(lvl + 1);
+  const indent = '  '.repeat(lvl);
   const stringifiedValue = _.isObjectLike(obj[key])
     ? `{\n${stringifyObject(obj[key], lvl + 2)}\n${indent}  }`
     : obj[key];
   return `${indent}  ${key}: ${stringifiedValue}`;
 }).join('\n');
 
-const stringifyTree = (tree, level = 0) => {
+const stringifyTree = (tree, level = 1) => {
   const stringified = tree.map((obj) => {
-    const indent = '  '.repeat(level + 1);
-    const stringfiedValue1 = _.isObjectLike(obj.obj1Value)
+    const indent = '  '.repeat(level);
+    const stringifiedValue1 = _.isObjectLike(obj.obj1Value)
       ? `{\n${stringifyObject(obj.obj1Value, level + 2)}\n${indent}  }`
       : `${obj.obj1Value}`;
-    const stringfiedValue2 = _.isObjectLike(obj.obj2Value)
+    const stringifiedValue2 = _.isObjectLike(obj.obj2Value)
       ? `{\n${stringifyObject(obj.obj2Value, level + 2)}\n${indent}  }`
       : `${obj.obj2Value}`;
 
     switch (obj.type) {
       case 'unmodified':
-        return `${indent}  ${obj.key}: ${stringfiedValue1}\n`;
+        return `${indent}  ${obj.key}: ${stringifiedValue1}\n`;
       case 'removed':
-        return `${indent}- ${obj.key}: ${stringfiedValue1}\n`;
+        return `${indent}- ${obj.key}: ${stringifiedValue1}\n`;
       case 'added':
-        return `${indent}+ ${obj.key}: ${stringfiedValue2}\n`;
+        return `${indent}+ ${obj.key}: ${stringifiedValue2}\n`;
       case 'modified':
         return [
-          `${indent}- ${obj.key}: ${stringfiedValue1}\n`,
-          `${indent}+ ${obj.key}: ${stringfiedValue2}\n`,
+          `${indent}- ${obj.key}: ${stringifiedValue1}\n`,
+          `${indent}+ ${obj.key}: ${stringifiedValue2}\n`,
         ];
       case 'nested':
         return [
