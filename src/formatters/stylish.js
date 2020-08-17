df0f5f2bcc34ee5a7f5d785c28119bee466b013d
melyssy <ksenyatokareva@gmail.com>
2020-08-17 23:52:35 +0300

    stringifyObject rework

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,21 +1,37 @@
 import _ from 'lodash';
 
-const stringifyObject = (obj, lvl) => _.keys(obj).map((key) => {
-  const indent = '  '.repeat(lvl);
-  const processedValue = _.isObjectLike(obj[key])
-    ? `{\n${stringifyObject(obj[key], lvl + 2)}\n${indent}  }`
-    : obj[key];
-  return `${indent}  ${key}: ${processedValue}`;
-}).join('\n');
+const indentSymbol = '  ';
+const openingBracket = '{';
+const closingBracket = '}';
+const step = 2;
+
+
+const stringifyObject = (obj, space, lvl) => {
+  const stringified = _.keys(obj).map((key) => {
+    const indent = space.repeat(lvl);
+
+    if (_.isObjectLike(obj[key])) {
+      const recursiveValue = stringifyObject(obj[key], ' ', lvl + step);
+      return `${indent}${key}: ${recursiveValue}`;
+    }
+
+    return `${indent}${key}: ${obj[key]}`;
+  }).join('\n');
+
+  const bracketIndent = (lvl - step) < 0 ? space.repeat(0) : space.repeat(lvl - step);
+  const closingBracketLine = `${bracketIndent}${closingBracket}`;
+
+  return [openingBracket, stringified, closingBracketLine].join('\n');
+};
 
 const stringifyTree = (tree, level = 1) => {
   const stringified = tree.map((obj) => {
-    const indent = '  '.repeat(level);
+    const indent = indentSymbol.repeat(level);
     const processedValue1 = _.isObjectLike(obj.obj1Value)
-      ? `{\n${stringifyObject(obj.obj1Value, level + 2)}\n${indent}  }`
+      ? `${openingBracket}\n${stringifyObject(obj.obj1Value, level + step)}\n${indent}  ${closingBracket}`
       : `${obj.obj1Value}`;
     const processedValue2 = _.isObjectLike(obj.obj2Value)
-      ? `{\n${stringifyObject(obj.obj2Value, level + 2)}\n${indent}  }`
+      ? `${openingBracket}\n${stringifyObject(obj.obj2Value, level + step)}\n${indent}  ${closingBracket}`
       : `${obj.obj2Value}`;
 
     switch (obj.type) {
@@ -32,7 +48,7 @@ const stringifyTree = (tree, level = 1) => {
         ];
       case 'nested':
         return [
-          `${indent}  ${obj.key}: {\n${stringifyTree(obj.children, level + 2)}`,
+          `${indent}  ${obj.key}: {\n${stringifyTree(obj.children, level + step)}`,
           `${indent}  }\n`,
         ];
       default:
